d7897f861e8a85b4e39dab1435ea13002609ec4c
Ryan <rycerveny@gmail.com>
2021-06-01 18:05:35 -0500

    Got Twitch Bot To Connect

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FunGuysCo_Twitch_Bot
+
+This is a fun lil thing where we can play with the twitch api and create a bot. I will use PIPENV, and the twitch libraries to do so.
+Any ideas are welcome. Mostly just trying to figure out and have fun.
---
+++ bot.py
@@ -0,0 +1,23 @@
+import os # for importing env vars for the bot to use
+from twitchio.ext import commands
+
+bot = commands.Bot(
+    # set up the bot
+    irc_token=os.environ['TMI_TOKEN'],
+    client_id=os.environ['CLIENT_ID'],
+    nick=os.environ['BOT_NICK'],
+    prefix=os.environ['BOT_PREFIX'],
+    initial_channels=[os.environ['CHANNEL']]
+)
+
+
+@bot.event
+async def event_ready():
+    'Called once when the bot goes online.'
+    print(f"{os.environ['BOT_NICK']} is online!")
+    ws = bot._ws  # this is only needed to send messages within event_ready
+    await ws.send_privmsg(os.environ['CHANNEL'], f"/me has landed!")
+
+if __name__ == "__main__":
+    print("BOT NICK:", os.environ['BOT_NICK'],)
+    bot.run()
